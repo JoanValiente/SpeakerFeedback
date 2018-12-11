7e35c5ec1ebf8c770d5f8c0fb83a7bfc150dd4fe
DavidTheMaaster <davidthegaamer@gmail.com>
2018-12-11 13:37:28 +0100

    Created class App

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/App.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/App.java
@@ -1,4 +1,28 @@
 package edu.upc.citm.android.speakerfeedback;
 
-public class App {
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application
+{
+    public static final String channel_ID = "SpeakerFeedback";
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        CreateNotificationChannels();
+    }
+
+    private void CreateNotificationChannels()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel(channel_ID, "SpeakerFeedbackChannel", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
 }
