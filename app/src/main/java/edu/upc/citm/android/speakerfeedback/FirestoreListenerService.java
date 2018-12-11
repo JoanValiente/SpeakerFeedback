9a930b8b9aa25211dc1d410f6baeb552b77c6784
DavidTheMaaster <davidthegaamer@gmail.com>
2018-12-11 13:56:15 +0100

    Created class FirestoreListenerService

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/FirestoreListenerService.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/FirestoreListenerService.java
@@ -1,4 +1,53 @@
 package edu.upc.citm.android.speakerfeedback;
 
-public class FirestoreListenerService {
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+public class FirestoreListenerService extends Service
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        createForegroundNotification();
+
+        return START_NOT_STICKY;
+    }
+
+    private void createForegroundNotification()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this,App.channel_ID)
+                .setContentTitle(String.format("Connected to 'testroom'"))
+                .setSmallIcon(R.drawable.ic_message)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
 }
