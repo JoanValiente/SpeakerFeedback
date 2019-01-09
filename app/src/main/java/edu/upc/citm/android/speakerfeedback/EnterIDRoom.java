f6d256e06963383605e3585b0351322c8ee7618b
DavidLozanoSánchez <davidthemaaster@gmail.com>
2019-01-09 21:52:10 +0100

    Starting access room using password

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/EnterIDRoom.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/EnterIDRoom.java
@@ -1,14 +1,29 @@
 package edu.upc.citm.android.speakerfeedback;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class EnterIDRoom extends AppCompatActivity {
     TextView enter_room_id;
+    private  String password_text = "";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -16,11 +31,84 @@ public class EnterIDRoom extends AppCompatActivity {
         setContentView(R.layout.activity_room_enter);
         enter_room_id = findViewById(R.id.room_ID);
     }
-
+    
     public void onClickEnterRoom(View view)
     {
-        if(enter_room_id.getText().toString().equals("")) {
+        final String roomID = enter_room_id.getText().toString();
+        
+        if(roomID.equals("")) {
             Toast.makeText(this, "Enter room ID:/", Toast.LENGTH_SHORT).show();
         }
+        else
+        {
+            db.collection("rooms").document(roomID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Log.i("SpeakerFeedback", documentSnapshot.toString());
+                    if(documentSnapshot.exists() && documentSnapshot.contains("open"))
+                    {
+                        if(documentSnapshot.contains("password") && !documentSnapshot.getString("passwird").isEmpty())
+                        {
+                            comparePassword(documentSnapshot.get("password").toString());
+                        }
+                    }
+                    else {
+
+                        if (!documentSnapshot.exists()) {
+                            Toast.makeText(EnterIDRoom.this,
+                                    "Room with ID " + "'" + roomID + ":" + " NOT EXIST", Toast.LENGTH_SHORT).show();
+                        } else if (!documentSnapshot.contains("open")) {
+                            Toast.makeText(EnterIDRoom.this,
+                                    "Room with ID " + "'" + roomID + "'" + " NOT OPEN", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EnterIDRoom.this,
+                            e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.e("SpeakerFeedback", e.getMessage());
+                }
+            });
+        }
     }
+    protected  void comparePassword(final String password)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter password:");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                password_text = input.getText().toString();
+                if (password_text.equals(password))
+                {
+                    Toast.makeText(EnterIDRoom.this, "Password correct", Toast.LENGTH_SHORT).show();
+
+                    Intent data = new Intent();
+                    data.putExtra("room_id", enter_room_id.getText().toString());
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
+                else
+                    Toast.makeText(EnterIDRoom.this, "Password incorrect!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
 }
--- app/src/main/java/edu/upc/citm/android/speakerfeedback/FirestoreListenerService.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/FirestoreListenerService.java
@@ -15,40 +15,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class FirestoreListenerService extends Service
-{
+public class FirestoreListenerService extends Service {
     private boolean connected = false;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        if(!connected)
-        {
-            createForegroundNotification();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!connected) {
+            String roomID = intent.getStringExtra("room");
+
+            if (!roomID.isEmpty()) {
+                db.collection("rooms").document(roomID)
+                        .collection("polls").whereEqualTo("open", true)
+                        .addSnapshotListener(pollListener);
+
+                createForegroundNotification(roomID);
+                connected = true;
+            }
         }
-
-        db.collection("rooms").document("testroom")
-                .collection("polls").whereEqualTo("open", true)
-                .addSnapshotListener(pollListener);
-
 
         return START_NOT_STICKY;
     }
 
-    private void createForegroundNotification()
-    {
+    private void createForegroundNotification(String room_ID) {
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-        Notification notification = new NotificationCompat.Builder(this,App.channel_ID)
-                .setContentTitle(String.format("Connected to 'testroom'"))
+        Notification notification = new NotificationCompat.Builder(this, App.channel_ID)
+                .setContentTitle(String.format("Connected to " + room_ID))
                 .setSmallIcon(R.drawable.ic_message)
                 .setContentIntent(pendingIntent)
                 .build();
@@ -58,19 +57,17 @@ public class FirestoreListenerService extends Service
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
     }
 
     @Nullable
     @Override
-    public IBinder onBind(Intent intent)
-    {
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
-    private EventListener<QuerySnapshot> pollListener = new EventListener<QuerySnapshot>(){
+    private EventListener<QuerySnapshot> pollListener = new EventListener<QuerySnapshot>() {
         @Override
         public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
             if (e != null) {
@@ -78,18 +75,16 @@ public class FirestoreListenerService extends Service
                 return;
             }
 
-            for (DocumentSnapshot doc : documentSnapshots)
-            {
+            for (DocumentSnapshot doc : documentSnapshots) {
                 Poll poll = doc.toObject(Poll.class);
-                if(poll.isOpen())
-                {
+                if (poll.isOpen()) {
                     Log.d("SpeakerFeedback", poll.getQuestion());
                     Intent intent = new Intent(FirestoreListenerService.this, MainActivity.class);
                     PendingIntent pendingIntent = PendingIntent.getActivity(FirestoreListenerService.this, 0, intent, 0);
-                    Notification notification = new NotificationCompat.Builder(FirestoreListenerService.this, App.channel_ID).setContentTitle("New poll: " +String.format(poll.getQuestion()))
+                    Notification notification = new NotificationCompat.Builder(FirestoreListenerService.this, App.channel_ID).setContentTitle("New poll: " + String.format(poll.getQuestion()))
                             .setSmallIcon(R.drawable.ic_message)
                             .setContentIntent(pendingIntent)
-                            .setVibrate(new long[] { 250, 250, 250, 250, 250 })
+                            .setVibrate(new long[]{250, 250, 250, 250, 250})
                             .setAutoCancel(true)
                             .build();
                     startForeground(1, notification);
@@ -97,4 +92,5 @@ public class FirestoreListenerService extends Service
             }
         }
     };
+
 }
